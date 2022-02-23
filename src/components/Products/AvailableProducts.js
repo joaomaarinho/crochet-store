203c777dc75a42cafc6adcd396a860983888b233
João Souza <joaomarinho.souza@hotmail.com>
2022-02-22 22:09:37 -0300

    firebase
    fetching products via http requests from firebase backend

--- src/components/Products/AvailableProducts.js
+++ src/components/Products/AvailableProducts.js
@@ -1,38 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import classes from "./AvailableProducts.module.css";
 import Card from "../UI/Card";
 import ProductItem from "./ProductItem/ProductItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Manta Jasmine Solteiro",
-    description: "Manta de croche ponto Jasmine verde água",
-    price: 234.99,
-  },
-  {
-    id: "m2",
-    name: "Tapete Tunisiano Pequeno",
-    description: "Tapete de croche ponto tunisiano",
-    price: 40,
-  },
-  {
-    id: "m3",
-    name: "Descanço de Mesa Circular",
-    description: "Descanço de mesa de croche retangular",
-    price: 45,
-  },
-  {
-    id: "m4",
-    name: "Almofada Jasmine",
-    description: "Capa almofada de croche ponto Jasmine",
-    price: 36,
-  },
-];
-
 function AvailableProducts() {
-  const productsList = DUMMY_MEALS.map((product) => (
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        "https://react-http-406dc-default-rtdb.firebaseio.com/products.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const responseData = await (await response).json();
+
+      const loadedProducts = [];
+
+      for (const key in responseData) {
+        loadedProducts.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setProducts(loadedProducts);
+      setIsLoading(false);
+    };
+
+    fetchProducts().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.ProductsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.ProductsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const productsList = products.map((product) => (
     <ProductItem
       id={product.id}
       key={product.id}
